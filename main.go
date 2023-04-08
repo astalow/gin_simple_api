f85195455c071e427c8cae6a011dfb81e9121111
astalow <endymion.0710@gmail.com>
2023-04-08 22:10:57 +0900

    dbto

--- main.go
+++ main.go
@@ -2,26 +2,50 @@ package main
 
 import (
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+type User struct {
+	Name  string `gorm:"primaryKey"`
+	Price int    `gorm:"not null"`
+}
+
 func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&User{})
+
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*.html")
+	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
-
-	r.POST("/submit", func(c *gin.Context) {
-		name := c.PostForm("name")
-		price := c.PostForm("price")
-		c.JSON(http.StatusOK, gin.H{
-			"name":  name,
-			"price": price,
+		rawHeaders, _ := httputil.DumpRequest(c.Request, false)
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Rawheader": string(rawHeaders),
 		})
 	})
 
-	r.Run()
+	r.POST("/", func(c *gin.Context) {
+		var user User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := db.Create(&user).Error; err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(200, user)
+	})
+
+	r.Run(":8080")
 }
